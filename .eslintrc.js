3a63c96ab0cbcca68f650d6b433255e22d1adaed
Yinggang.Dong <dyggod@163.com>
2022-11-22 15:26:55 +0800

    配置eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'vue/multi-word-component-names': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        tsx: 'never',
+        ts: 'never',
+        vue: 'never',
+      },
+    ],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+};
---
+++ examples/.eslintrc.js
@@ -0,0 +1,5 @@
+// 引入上一层eslint配置
+
+module.exports = {
+  extends: ['@dgui/.eslintrc.js'],
+};
