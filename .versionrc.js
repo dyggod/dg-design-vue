dbd3ad51c9e5be8b37cf88b006deecfdb1d84cd5
Yinggang.Dong <dyggod@163.com>
2022-11-29 14:25:08 +0800

    ci: 完善发布构建

---
+++ .versionrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  header: '密码服务平台-前端 更新历史记录',
+  types: [
+    { type: 'feat', section: '✨ Features | 新功能' },
+    { type: 'fix', section: '🐛 Bug Fixes | Bug 修复' },
+    { type: 'init', section: '🎉 Init | 初始化' },
+    { type: 'docs', section: '✏️ Documentation | 文档' },
+    { type: 'style', section: '💄 Styles | 代码风格或CSS样式' },
+    { type: 'refactor', section: '♻️ Code Refactoring | 代码重构' },
+    { type: 'perf', section: '⚡ Performance Improvements | 性能优化' },
+    { type: 'test', section: '✅ Tests | 测试' },
+    { type: 'revert', section: '⏪ Revert | 回退', hidden: true },
+    { type: 'build', section: '📦‍ Build System | 打包构建' },
+    { type: 'chore', section: '🚀 Chore | 构建/工程依赖/工具' },
+    { type: 'ci', section: '👷 Continuous Integration | CI 配置' },
+  ],
+};
---
+++ scripts/release.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const mainPackage = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'));
+const componentsPackageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../packages/components/package.json'), 'utf8'),
+);
+const nowVersion = mainPackage.version;
+
+const VersionType = ['patch', 'minor', 'major'];
+
+/**
+ * 根据alias是patch、minor、major，计算下一个版本号
+ * @param {alias} type
+ */
+function generateVersion(alias) {
+  if (!VersionType.includes(alias)) {
+    throw new Error(`请传入正确的版本号类型，当前传入的是${alias}，可选的版本号类型为${VersionType.join('、')}`);
+  }
+  const versionArr = nowVersion.split('.');
+  const [major, minor, patch] = versionArr;
+  switch (alias) {
+    case 'patch':
+      versionArr[2] = parseInt(patch, 10) + 1;
+      break;
+    case 'minor':
+      versionArr[1] = parseInt(minor, 10) + 1;
+      versionArr[2] = 0;
+      break;
+    case 'major':
+      versionArr[0] = parseInt(major, 10) + 1;
+      versionArr[1] = 0;
+      versionArr[2] = 0;
+      break;
+    default:
+      break;
+  }
+  return versionArr.join('.');
+}
+
+// 接受npm run 命令的参数, 计算下一个版本号的函数
+const args = process.argv.slice(2);
+const [type] = args;
+const nextVersion = generateVersion(type);
+
+// 同步至packages/components下的package.json
+componentsPackageJson.version = nextVersion;
+fs.writeFileSync(path.resolve(__dirname, '../packages/components/package.json'), JSON.stringify(componentsPackageJson, null, 2));
+
+console.log('\x1b[33m%s\x1b[0m', '开始构建组件库...');
+execSync('npm run build', { cwd: path.resolve(__dirname, '../packages/components'), stdio: 'inherit' });
+console.log('\x1B[32m%s\x1B[0m', '组件库构建完成');
+
+// 发布npm包
+console.log('\x1b[33m%s\x1b[0m', '开始发布组件库...');
+execSync(
+  'npm publish',
+  { cwd: path.resolve(__dirname, '../packages/components'), stdio: 'inherit' },
+);
+console.log('\x1B[32m%s\x1B[0m', '组件库发布完成');
+
+console.log('\x1b[33m%s\x1b[0m', '开始升级版本号并更新changelog...');
+execSync(
+  `standard-version --release-as ${type}`,
+  { cwd: path.resolve(__dirname, '../'), stdio: 'inherit' },
+);
+console.log('\x1B[32m%s\x1B[0m', '版本升级完成');
+
+console.log('\x1b[33m%s\x1b[0m', '开始push代码...');
+execSync('git push --follow-tags', { cwd: path.resolve(__dirname, '../'), stdio: 'inherit' });
+console.log('\x1B[32m%s\x1B[0m', '代码push完成');
