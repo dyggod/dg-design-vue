d413fe4175d8ea160a27185e4a364685193374ad
Yinggang.Dong <dyggod@163.com>
2022-12-09 15:37:54 +0800

    feat: 星灯开关

---
+++ docs/docs/components/switch/light.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <dg-switch
+      v-model="switchValue"
+      dg-fun="light"
+    />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+const switchValue = ref(false);
+</script>
+
+<style>
+
+</style>
