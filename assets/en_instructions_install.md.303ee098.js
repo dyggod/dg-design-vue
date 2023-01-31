dd11316994e6c4cd8fa9afe456066a8a17a3ff6b
dyggod <dyggod@163.com>
2023-01-31 06:49:37 +0000

    Deploying to gh-pages from  @ fdf6966f7c8a8b46798e85b59bb377580e89e641 🚀

---
+++ assets/en_instructions_install.md.303ee098.js
@@ -0,0 +1,11 @@
+import{_ as s,o as n,c as a,e as l}from"./app.dfc2d51f.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":2,"title":"Install","slug":"install","link":"#install","children":[]}],"relativePath":"en/instructions/install.md","lastUpdated":1675147720000}'),e={name:"en/instructions/install.md"},t=l(`<h2 id="install" tabindex="-1">Install <a class="header-anchor" href="#install" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#A6ACCD;"># Choose a package manager you like</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># NPM</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ npm install dg-design-vue --save</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># Yarn</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ yarn add dg-design-vue</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># pnpm</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ pnpm install dg-design-vue</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,2),p=[t];function o(c,i,r,d,C,_){return n(),a("div",null,p)}const u=s(e,[["render",o]]);export{h as __pageData,u as default};
---
+++ assets/zh_instructions_install.md.e2a2ddfa.js
@@ -0,0 +1,11 @@
+import{_ as s,o as n,c as a,e}from"./app.dfc2d51f.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":2,"title":"安装","slug":"安装","link":"#安装","children":[]}],"relativePath":"zh/instructions/install.md","lastUpdated":1675147720000}'),l={name:"zh/instructions/install.md"},p=e(`<h2 id="安装" tabindex="-1">安装 <a class="header-anchor" href="#安装" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#A6ACCD;"># 选择一个你喜欢的包管理器</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># NPM</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ npm install dg-design-vue --save</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># Yarn</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ yarn add dg-design-vue</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># pnpm</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ pnpm install dg-design-vue</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,2),t=[p];function o(c,i,r,d,_,C){return n(),a("div",null,t)}const u=s(l,[["render",o]]);export{h as __pageData,u as default};
